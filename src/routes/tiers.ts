7df59122e3d9ab55d56999fd490fe177f11ae5f6
ameerthehacker <ameerjhanprof@gmail.com>
2021-01-26 17:49:14 +0530

    feat: update repos api

--- src/routes/tiers.ts
+++ src/routes/tiers.ts
@@ -7,6 +7,10 @@ type CreateTierRequest = {
   minAmount: string;
 };
 
+type AddRepositoryRequest = {
+  name: string;
+}
+
 async function validateCreateTierRequest(req: Request<{}, {}, CreateTierRequest>, res: Response, cb: () => void) {
   const { title, description, minAmount } = req.body;
 
@@ -17,14 +21,28 @@ async function validateCreateTierRequest(req: Request<{}, {}, CreateTierRequest>
   }
 }
 
+async function validateAddRepositoryRequest(req: Request<{}, {}, AddRepositoryRequest>, res: Response, cb: () => void) {
+  const { name } = req.body;
+
+  if (!name) {
+    res.sendStatus(400);
+  } else {
+    await cb();
+  }
+}
+
 export default function getTierRoutes(db: PrismaClient) {
   const tierRoutes = express.Router();
 
   tierRoutes.get('/', async (req, res) => {
-    const user = req.user as User;
-    const tiers = await db.tier.findMany({ where: { user } });
-  
-    res.json({ tiers });
+    try {
+      const user = req.user as User;
+      const tiers = await db.tier.findMany({ where: { user }, include: { repositories: true } });
+    
+      res.json({ tiers });
+    } catch {
+      res.sendStatus(500);
+    }
   });
   
   tierRoutes.post('/', async (req: Request<{}, {}, CreateTierRequest>, res) => {
@@ -32,20 +50,67 @@ export default function getTierRoutes(db: PrismaClient) {
       const { title, description, minAmount } = req.body;
       const user = req.user as User;
   
-      await db.tier.create({
-        data: {
-          title,
-          description,
-          minAmount,
-          user: {
-            connect: {
-              id: user.id
+      try {
+        await db.tier.create({
+          data: {
+            title,
+            description,
+            minAmount,
+            user: {
+              connect: {
+                id: user.id
+              }
             }
           }
-        }
-      });
-  
-      res.sendStatus(201);
+        });
+    
+        res.sendStatus(201);
+      } catch {
+        res.sendStatus(500);
+      }
+    });
+  });
+
+  tierRoutes.post('/:id/repositories', async (req: Request<{ id: string }, {}, AddRepositoryRequest>, res) => {
+    await validateAddRepositoryRequest(req, res, async () => {
+      try {
+        const user = req.user as User;
+        const tierId = req.params.id;
+        const { name } = req.body;
+
+        await db.repository.upsert({
+          create: {
+            name,
+            user: {
+              connect: {
+                id: user.id
+              }
+            },
+            tier: {
+              connect: {
+                id: parseInt(tierId)
+              }
+            }
+          },
+          update: {
+            tier: {
+              connect: {
+                id: parseInt(tierId)
+              }
+            }
+          },
+          where: {
+            userId_name: {
+              name,
+              userId: user.id
+            }
+          }
+        });
+
+        res.sendStatus(201);
+      } catch {
+        res.sendStatus(500);
+      }      
     });
   });
   
