1991fc07035f2ed7017121746415bdafe31180a5
ameerthehacker <ameerjhanprof@gmail.com>
2021-01-28 16:01:14 +0530

    ref: update response format

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -9,6 +9,7 @@ export default function getCorsMiddleware() {
   // setup cors for protected routes
   const corsMiddleware = cors({
     allowedHeaders: '*',
+    methods: ['POST', 'DELETE', 'GET'],
     origin: isProd? (origin, callback) => {
       if (origin && corsDomains.includes(origin)) {
         callback(null, true);
